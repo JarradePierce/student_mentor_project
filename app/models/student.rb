5bc6f682135fab67045cddb5a7ed4e3d5e13d2e8
Yun Jae Chang <yunjaechang@Yuns-MacBook-Air.local>
2017-07-20 19:07:05 -0700

    Add css

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   has_secure_password
-  
+
   has_many :appointments
   has_many :mentors, through: :appointments
   has_many :ratings, as: :feedbackable, foreign_key: "rater_id"
@@ -9,4 +9,5 @@ class Student < ApplicationRecord
   validates :username, :email, :password_digest, :phase, presence: true
 
   validates :username, uniqueness: true
+
 end
